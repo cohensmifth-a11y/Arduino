a188ed4d1e2dcb7e4f32823eec2b61231ce768f7
Cohen Smith <cohensmifth@gmail.com>
2026-02-17 14:13:29 -0500

    added police light delay

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -29,12 +29,15 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(3000);                      // wait for a second
+  delay(500);                      // wait for a second
   digitalWrite(10, LOW);   // turn the LED off by making the voltage LOW
-  delay(2000);                      // wait for a second
+    digitalWrite(9, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(500);                      // wait for a second
+  digitalWrite(9, LOW);   // turn the LED off by making the voltage LOW
 }
