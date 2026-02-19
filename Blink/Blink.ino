0357f48cc1be81033ceb1110203a3b6def774cad
Cohen Smith <cohensmifth@gmail.com>
2026-02-19 13:56:53 -0500

    Added PMW Light

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -24,28 +24,50 @@
 
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
-
 int buzzer = 11;
+int fadeLED = 6;
+
+int brightness = 0;
+int fadeAmount = 5;
 
 void setup() {
   pinMode(buzzer, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
+  pinMode(fadeLED, OUTPUT);
 }
 
 void loop() {
 
-  // Siren going up (lower pitch range)
+  // Siren going up (lower pitch)
   for (int freq = 400; freq <= 900; freq += 8) {
 
     tone(buzzer, freq);
 
+    // Flashing police lights
     digitalWrite(10, HIGH);
     digitalWrite(9, LOW);
+
+    // Fade LED
+    analogWrite(fadeLED, brightness);
+    brightness += fadeAmount;
+
+    if (brightness <= 0 || brightness >= 255) {
+      fadeAmount = -fadeAmount;
+    }
+
     delay(40);
 
     digitalWrite(10, LOW);
     digitalWrite(9, HIGH);
+
+    analogWrite(fadeLED, brightness);
+    brightness += fadeAmount;
+
+    if (brightness <= 0 || brightness >= 255) {
+      fadeAmount = -fadeAmount;
+    }
+
     delay(40);
   }
 
@@ -56,13 +78,30 @@ void loop() {
 
     digitalWrite(10, HIGH);
     digitalWrite(9, LOW);
+
+    analogWrite(fadeLED, brightness);
+    brightness += fadeAmount;
+
+    if (brightness <= 0 || brightness >= 255) {
+      fadeAmount = -fadeAmount;
+    }
+
     delay(40);
 
     digitalWrite(10, LOW);
     digitalWrite(9, HIGH);
+
+    analogWrite(fadeLED, brightness);
+    brightness += fadeAmount;
+
+    if (brightness <= 0 || brightness >= 255) {
+      fadeAmount = -fadeAmount;
+    }
+
     delay(40);
   }
 }
+
 
 
 
